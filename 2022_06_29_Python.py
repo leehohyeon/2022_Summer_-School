ddabb94ba83ead2839022fd21c656b3a3829389e
leehohyeon <50910208+leehohyeon@users.noreply.github.com>
2022-06-30 10:04:04 +0900

    Update 2022_06_29_Python.py

--- 2022_06_29_Python.py
+++ 2022_06_29_Python.py
@@ -4,5 +4,5 @@ for x in range(1, 101):
     if x % 2 == 0: e_tot += x
     else: o_tot += x
 
-print("1~100까지 짝수 합 : {0}, 홀수 합 : {1}" .format(e_tot, o_tot)
+print("1~100까지 짝수 합 : {0}, 홀수 합 : {1}" .format(e_tot, o_tot))
 
